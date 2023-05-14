05f44fa7c0af7bbce55207d5a1e77748e17349a3
emmanuel dodoo <end25@cornell.edu>
2023-05-14 16:57:33 -0400

    Added nav bar shadow and nav bar scroll effect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 
+// Sticky-ish navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
 // Hamburger button animation and functionality
 const hamburgerButton = document.querySelector(".mobile-nav-toggle");
 const navList = document.querySelector(".nav-list");
